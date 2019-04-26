3057e82a8e5b0124ebfaf41b102cc8f9d6091502
Nola Donato <nola.donato@samsung.com>
2019-04-26 13:33:41 -0700

    add -lz to linker flags

--- intel_debug_build32.sh
+++ intel_debug_build32.sh
@@ -9,6 +9,7 @@ $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DANDROID_FORCE_ARM_BUILD=TRUE \
 	-DCMAKE_INSTALL_PREFIX=install \
 	-DANDROID_STL=c++_shared \
+	-DANDROID_LINKER_FLAGS=-lz \
 	-DANDROID_STL_FORCE_FEATURES=ON \
 	-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake \
 	-DANDROID_TOOLCHAIN=clang
